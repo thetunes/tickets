52ff94968f92c27315170c09e8509048c776a9b5
HeroBuxx <me@herobuxx.me>
2024-01-04 22:54:10 +0700

    ticket: Fix bugs
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/js/getTicket.js
+++ src/js/getTicket.js
@@ -1,3 +1,6 @@
+import { token } from "./controller/cookies.js";
+import { deleteCookie } from "https://jscroot.github.io/cookie/croot.js";
+
 function getTicket() {
     var requestOptions = {
         method: 'GET',
@@ -50,4 +53,44 @@ function displayTickets(tickets) {
     });
 }
 
-window.onload = getTicket;
+// Function to remove the 'token' cookie
+function removeTokenCookie() {
+  deleteCookie('token');
+  window.location.reload();
+}
+
+// Update the showProtectedNav function to include the Log Out link with the click event
+function showProtectedNav() {
+  const tableBody = document.getElementById('protectedNavLinks');
+  tableBody.innerHTML = '';
+  const navLink = document.createElement('li');
+
+  navLink.className = "link";
+
+  if (token === "") {
+    navLink.innerHTML = `
+    <a href="/login">Login</a>
+    `;
+  } else {
+    navLink.innerHTML = `
+    <a href="my_order.html">My Orders</a>
+    <a href="#" id="logoutLink">Log Out</a>
+    `;
+  }
+
+  tableBody.appendChild(navLink);
+
+  // Attach an event listener to the Log Out link
+  const logoutLink = document.getElementById('logoutLink');
+  if (logoutLink) {
+    logoutLink.addEventListener('click', removeTokenCookie);
+  }
+}
+
+
+function initialize() {
+    showProtectedNav();
+    getTicket();
+}
+
+window.onload = initialize;
