9014fe6a8a9c947a36c4bc12b41881c51cf06a8f
HeroBuxx <me@herobuxx.me>
2024-01-04 22:47:50 +0700

    ticket: Add protected navbar
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/js/guard.js
+++ src/js/guard.js
@@ -2,4 +2,19 @@ import { token } from "./controller/cookies.js";
 
 if (token === "") {
   window.location.replace("http://tunes.herobuxx.me/login/");
+  showProtectedNav();
+}
+
+
+function showProtectedNav() {
+  const tableBody = document.getElementById('protectedNavLinks');
+  tableBody.innerHTML = '';
+
+  const navLink = document.createElement('li');
+  navLink.className = "link";
+  navLink.innerHTML = `
+  <a href="/login">Login</a>
+  <a href="/logout">logout</a>
+  `;
+  tableBody.appendChild(card);
 }
