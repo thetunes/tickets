b5dd03fd62babb89472fb984f04941f34adbcf0d
HeroBuxx <me@herobuxx.me>
2024-01-17 09:15:01 +0700

    tickets: Finishing touch for payment receipt upload
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/js/getOrders.js
+++ src/js/getOrders.js
@@ -57,32 +57,67 @@ function displayTickets(tickets) {
 
     getUserId().then(userID => { // Use getUserId as a Promise
         tickets.filter(ticket => ticket.userid === userID).forEach(ticket => {
-            if (ticket.status === "true") {
-                var orderStatus = "Completed";
-            } else if (ticket.status === "false") {
-                var orderStatus = "Canceled";
-            } else {
-                var orderStatus = "Waiting for confirmation";
-            }
-
             const card = document.createElement('div');
-            card.innerHTML = `
-            <section class="section__container popular__container">
-                <h1 class="section__header">${ticket.ticketid}</h1>
-                <div class="popular__grid text-white">
-                    <h2>Ticket ID</h2>
-                    <p>${ticket.ticketid}</p>
+            if (ticket.status === "true") {
+                card.innerHTML = `
+                <section class="section__container popular__container">
+                    <h1 class="section__header">${ticket.ticketid}</h1>
+                    <div class="popular__grid text-white">
+                        <h2>Ticket ID</h2>
+                        <p>${ticket.ticketid}</p>
+                    </div>
+                    <div class="popular__grid text-white">
+                        <h2>Ordered at</h2>
+                        <p>${ticket.CreatedAt}</p>
+                    </div>
+                    <div class="popular__grid text-white">
+                        <h2>Status</h2>
+                        <p>Completed</p>
+                    </div>
+                </section>
+                `;
+            } else if (ticket.status === "false") {
+                card.innerHTML = `
+                <section class="section__container popular__container">
+                    <h1 class="section__header">${ticket.ticketid}</h1>
+                    <div class="popular__grid text-white">
+                        <h2>Ticket ID</h2>
+                        <p>${ticket.ticketid}</p>
+                    </div>
+                    <div class="popular__grid text-white">
+                        <h2>Ordered at</h2>
+                        <p>${ticket.CreatedAt}</p>
+                    </div>
+                    <div class="popular__grid text-white">
+                        <h2>Status</h2>
+                        <p>Cancelled</p>
+                    </div>
+                </section>
+                `;
+            } else {
+                card.innerHTML = `
+                <section class="section__container popular__container">
+                    <h1 class="section__header">${ticket.ticketid}</h1>
+                    <div class="popular__grid text-white">
+                        <h2>Ticket ID</h2>
+                        <p>${ticket.ticketid}</p>
+                    </div>
+                    <div class="popular__grid text-white">
+                        <h2>Ordered at</h2>
+                        <p>${ticket.CreatedAt}</p>
+                    </div>
+                    <div class="popular__grid text-white">
+                        <h2>Status</h2>
+                        <p>Waiting for payment</p>
+                    </div>
+                    <div class="btn-container">
+                    <a href="payment.html?id=${ticket.id}">
+                        <button id="buyBtn" class="btn-main pt-8">Upload Payment Receipt</button>
+                    </a>    
                 </div>
-                <div class="popular__grid text-white">
-                    <h2>Ordered at</h2>
-                    <p>${ticket.CreatedAt}</p>
-                </div>
-                <div class="popular__grid text-white">
-                    <h2>Status</h2>
-                    <p>${orderStatus}.</p>
-                </div>
-            </section>
-            `;
+                </section>
+                `;
+            }
             tableBody.appendChild(card);  // Use 'card' instead of 'row'
         });
     });
